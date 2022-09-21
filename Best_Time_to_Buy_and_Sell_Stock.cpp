de2026f6234e966319b963062dd2d880ed1ad5db
ABHI8769 <109608167+ABHI8769@users.noreply.github.com>
2022-09-21 23:20:46 +0530

    Array Problems Solved

--- Best_Time_to_Buy_and_Sell_Stock.cpp
+++ Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -1,7 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-
-    return 0;
-}
+class Solution {
+public:
+    int maxProfit(vector<int>& price) {
+        int curr_price = INT_MAX;
+        int profit = 0;
+        
+        for(int i=0;i<price.size();i++){
+            if(price[i]<curr_price){
+                curr_price = price[i];
+            }
+            else if(price[i]-curr_price > profit){
+                profit = price[i] - curr_price;
+            }
+        }
+        return profit;
+    }
+};
